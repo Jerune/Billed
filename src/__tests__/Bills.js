d9cedafda6b0e1843423dd12e59bff34f5422e9e
Jeroen van de Sanden <vandesanden@hotmail.com>
2022-09-20 10:41:46 +0200

    refactor: Changed Ordered Bills Test to function correctly
    Bills ordering test now uses list of bills from getBills function instead of screen, which fixes the unit test

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -5,8 +5,10 @@
 import {screen, waitFor} from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
-import { ROUTES_PATH} from "../constants/routes.js";
+import { ROUTES, ROUTES_PATH} from "../constants/routes.js";
 import {localStorageMock} from "../__mocks__/localStorage.js";
+import Bills from '../containers/Bills.js'
+import mockStore from "../__mocks__/store"
 
 import router from "../app/Router.js";
 
@@ -28,9 +30,17 @@ describe("Given I am connected as an employee", () => {
       //to-do write expect expression
 
     })
-    test("Then bills should be ordered from earliest to latest", () => {
-      document.body.innerHTML = BillsUI({ data: bills })
-      const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
+    test("Then bills should be ordered from earliest to latest", async () => {
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+      
+      const billsContainer = new Bills({
+        document, onNavigate, store: mockStore, localStorage: null
+      })
+      const result = await billsContainer.getBills()
+      const dates = result.map(bill => bill.date)
+
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
